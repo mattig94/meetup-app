9eda38c50d1b26613a9ce1b5e2b712ac6367064c
mattig94 <mattig94@users.noreply.github.com>
2020-01-09 10:27:40 -0500

    tests for feature 3

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = {
+    number: 32,
+  }
+  
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ number: value });
+  }
+
   render() {
     return(
       <div className="NumberOfEvents">
-
+        <p>Show
+          <input type="number" value={this.state.number} onChange={this.handleInputChanged} />
+          Events
+        </p>
       </div>
     );
   }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -11,4 +11,18 @@ describe('<NumberOfEvents /> component', () => {
   test('test that component is rendered', () => {
     expect(NumberOfEventsWrapper).toHaveLength(1);
   });
+
+  test('render event numbrt input', () => {
+    expect(NumberOfEventsWrapper.find('input')).toHaveLength(1);
+  });
+
+  test('default number of events is 32', () => {
+    expect(NumberOfEventsWrapper.state('number')).toBe(32);
+  });
+
+  test('change number state when input changes', () => {
+    const eventObject = { target: { value: 5 }};
+    NumberOfEventsWrapper.find('input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('number')).toBe(5);
+  })
 })
